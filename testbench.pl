07550ac2672ad14cb32dd6b09405fe0d60974490
scott <scott@scott-laptop.(none)>
2010-11-05 00:23:49 -0400

    Testbench

---
+++ testbench.pl
@@ -0,0 +1,66 @@
+#!/usr/bin/perl
+print "\n=================VERISHORT TESTBENCH==================\n\n";
+
+#Counters
+$totalTests = 0;
+$passedTests = 0;
+
+#Get every test file into an array
+@verishorts=`ls test/*.vs`;
+
+#For every test file, test it
+foreach $verishortTestFile (@verishorts) {
+	#Take out the whitespace in the filename
+	chomp $verishortTestFile;
+	
+	#Get the translated and goal filenames
+	$originalVerishortTestFile = $verishortTestFile;
+	$verishortTestFile =~  s/\.vs/\.v/;
+	$translated = $verishortTestFile;
+	$verishortTestFile =~ s/\.v/Goal\.v/;
+	$goal = $verishortTestFile;
+
+	#Translate the verishort
+	$translate = "./vsc $originalVerishortTestFile";
+	print "$translate\n";
+	`$translate`;
+		
+	#Test the differences between the file.  If there are no differences, that test was passed
+	$test = "diff $translated $goal";
+	print "$test\n";
+	$diffResult = `$test`;
+	print "$diffResult";
+	if ($diffResult =~ m/^$/) {
+		print "Translated file matches goal: $originalVerishortTestFile\n";
+		$passedTests++;
+		}
+	else {
+		print "Translated file does not match goal: $originalVerishortTestFile\n";
+		}
+	
+	
+	#Test that the file compiles correctly
+	#$compile = "iverilog $translated -s $translated";
+	#print "$compile\n";
+	#$compileResult = `$compile`;
+	#print "$compileResult";
+	
+	
+	#Remove the compiled file
+	$remove = "rm $translated";
+	print "$remove\n";
+	`$remove`;
+	
+	#Increment
+	$totalTests++;
+	}
+
+#Print final messages
+print "==========================\n";
+if ($passedTests == $totalTests) {
+	print "PASSED ALL TESTS!\n";
+	}
+else {
+	print "Failed atleast one test\n";
+	}
+print "Passed: $passedTests/$totalTests\n";
