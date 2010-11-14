4aac2adb332e35920e90a74c68ffc64a7998a865
Tim Song <rs2740@gmail.com>
2010-11-13 19:15:27 -0500

    Fix ast.mli - reorder types

--- ast.mli
+++ ast.mli
@@ -1,41 +1,6 @@
 type op = Plus | Minus | Multiply | Divide | Modulus | Eq | Ne | Ge | Gt | Le | Lt | And | Or | Xor | Nand | Nor | Xnor | Lshift | Rshift
 
-
-type moddecl= {
-	modname : string; (* Name of the module *)
-	inputs : string list;
-	outputs : string list;
-	statements : statement list;
-	parameters : parameter list;
-	declarations: declaration list;
-	returnwidth: int;
-	}
-
-type program = mod_decl list
-
-type parameter = string * int	
-
-type statement = 
-    Nop
-  | Expr of expr
-  | Block of statement list
-  | If of expr * statement * statement
-  | Case of lvalue * case_item list
-  | Return of expr
-  | For of expr * expr * expr * statement
-
-type case_item = string * statement
-
-type decl_type = Wire | Reg
-
-type declaration = {
-	decltype : decl_type;
-	declname : string;
-	declwidth: int;
-	init : expr;
-}
-
-type binding = lvalue * expr
+type parameter = string * int
 
 type expr =
     DLiteral of int
@@ -49,21 +14,51 @@ type expr =
   | Inst of string * binding list * binding list
   | Reset
   | Noexpr
-
-type concat_item = 
+and concat_item = 
      ConcatBLiteral of string
    | ConcatLvalue of lvalue
    | ConcatDuplBLiteral of int * string
    | ConcatDuplLvalue of int * lvalue
-
-type lvalue = 
+and lvalue = 
     Identifier of string
   | Subscript of string * expr
   | Range of string * expr * expr
+and binding = lvalue * expr
+
+
+type statement = 
+    Nop
+  | Expr of expr
+  | Block of statement list
+  | If of expr * statement * statement
+  | Case of lvalue * case_item list
+  | Return of expr
+  | For of expr * expr * expr * statement
+
+and case_item = string * statement
+
+type decl_type = Wire | Reg
+
+type declaration = {
+	decltype : decl_type;
+	declname : string;
+	declwidth: int;
+	init : expr;
+}
 
 type id_with_width = {
 	id : string;
 	width : int;
 }
 
+type moddecl= {
+	modname : string; (* Name of the module *)
+	inputs : string list;
+	outputs : string list;
+	statements : statement list;
+	parameters : parameter list;
+	declarations: declaration list;
+	returnwidth: int;
+}
 
+type program = mod_decl list
