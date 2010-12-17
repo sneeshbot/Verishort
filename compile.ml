5fe9900c7906226d7db2c925b1ad3e257ea8c4d1
Anish <anish.bramhandkar@gmail.com>
2010-12-17 13:02:05 -0500

    added functions to check local existence, get parameter if exists, get arg if exists

--- compile.ml
+++ compile.ml
@@ -6,7 +6,7 @@ module StringMap = Map.Make(String)
 type enviro = {
     local_map    : declaration list StringMap.t; (* FP offset for args, locals *)
 	param_map    : parameter list StringMap.t;
-	arg_map     : (id_with_width list * id_with_width list) StringMap.t;
+	arg_map      : (id_with_width list * id_with_width list) StringMap.t;
   }
 
 let string_map_args map mods = 
@@ -17,6 +17,23 @@ let string_map_params map mods =
 
 let string_map_locals map mods = 
   List.fold_left (fun m mod1 -> StringMap.add mod1.modname mod1.declarations m) map mods
+  
+let local_exist mod_name local_name env = 
+  try List.mem local_name (StringMap.find mod_name env.local_map) with Not_found -> false
+    
+let get_param mod_name param_name env = 
+  List.find param_name (StringMap.find mod_name env.param_map)
+
+let rec get_arg_tuple name lst =
+  match lst with
+      [] -> raise Not_found
+    | (s, i, _)::tl -> if s = name then (s,i) else get_arg_tuple name tl  
+  
+let get_arg mod_name arg_name env = 
+  let tuples = StringMap.find mod_name env.arg_map in
+  get_arg_tuple arg_name((fst tuples) @ (snd tuples) )
+  
+  
 
 let check_modnames lst1 mods = 
   List.fold_left (fun lst mod1 -> if List.mem mod1.modname lst then raise (Parse_Failure("Duplicate module name." , mod1.modpos)) else mod1.modname :: lst) lst1 mods
